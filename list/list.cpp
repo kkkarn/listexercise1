cfd6f322a9312c9b79d048358e1be4ef5f10c1e4
ze.mandkarn@gmail.com <ze.mandkarn@gmail.com>
2015-08-26 09:54:45 +0700

    inittial commit

--- list/list.cpp
+++ list/list.cpp
@@ -20,7 +20,17 @@ void List::pushToHead(char el)
 }
 void List::pushToTail(char el)
 {
-	//TO DO!
+	Node *tmp = new Node(el);
+	if (head == 0)
+	{
+		tail = tmp;
+		head = tmp;
+	}
+	else
+	{
+		tail->next = tmp;
+		tail = tmp;
+	}
 }
 char List::popHead()
 {
@@ -39,18 +49,68 @@ char List::popHead()
 }
 char List::popTail()
 {
-	// TO DO!
-	return NULL;
+	if (tail == NULL)
+	{
+		return NULL;
+	}
+	
+	else
+	{
+		char el = tail->data;
+		Node *tmp = tail;
+		if (head == tail) 
+		{
+			head = NULL;
+			tail = NULL;
+		}
+		else
+		{
+			Node *Tail = head;
+			while (Tail->next != tail)
+				Tail = Tail->next;
+			tail = Tail;
+			tail->next = NULL;
+		}
+		delete tmp;
+		
+	}
 }
 bool List::search(char el)
 {
+	Node *tmp = head;
+	while (tmp != tail)
+	{
+		if (tmp->data == el)
+			return true;
+		tmp = tmp->next;
+	}
 	// TO DO! (Function to return True or False depending if a character is in the list.
-	return NULL;
+	return false;
 }
 void List::reverse()
 {
+		if (head != NULL) 
+		{	if (head != tail)
+			{
+			Node *prev = NULL, *n = NULL, *current;
+			current = head;
+			while (current != NULL) 
+			{
+				if (current->next == NULL)
+				{
+					tail = head;
+				}
+				n = current->next;
+				current->next = prev;
+				prev = current;
+				current = n;
+			}
+			head = prev;
+			}
+		}
 	// TO DO! (Function is to reverse the order of elements in the list.
 }
+
 void List::print()
 {
 	if(head  == tail)
--- list/program.cpp
+++ list/program.cpp
@@ -9,6 +9,13 @@ void main()
 	mylist.pushToHead('k');
 	mylist.pushToHead('e');
 	mylist.pushToHead('n');
+	mylist.pushToTail('k');
+	mylist.pushToTail('e');
+	mylist.pushToTail('n');
+	mylist.reverse();
+	mylist.search('k');
+	mylist.popHead();
+	mylist.popTail();
 	mylist.print();
 
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, 
